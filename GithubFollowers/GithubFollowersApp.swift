d9358b3be20bd45dbcd82afda39df9fb9bdf00f0
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-10-09 22:59:23 -0400

    Initial Commit

---
+++ GithubFollowers/GithubFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubFollowersApp.swift
+//  GithubFollowers
+//
+//  Created by Cesario Nivar on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
